a639326448f9137b2333eff2964c0d4e3b73b556
Chip Lempke <clempke90@gmail.com>
2020-12-07 21:59:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,28 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
-  #
   # Print a custom greeting for each element
+  array.each do |element|
+  puts "Hello #{element}!"
+end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  #
   # Print a numbered list of each element
+  counter = 1
+
+  array.count do |element|
+    if counter.is_a? Integer
+    puts "#{counter}. #{element}"
+    counter += 1
+  end
 end
+end
+
+total = 0
+[1,2,3,4].each do |num|
+  if num.even?
+    total += 1
+  end
+end
+total
